cd181d3d47262ca847e62c8522beebfea9b432e4
pranavvdesai <pranavdesai.psd@gmail.com>
2021-11-27 22:59:15 +0530

    feat: auth

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import BarChart from '../components/BarChart';
 
 function Dashboard() {
+  if (
+    localStorage.getItem("HH") === null ||
+    localStorage.getItem("HH") === ""
+  ) {
+    window.location.href = "/";
+  }
   return (
     <div>
             <BarChart />
