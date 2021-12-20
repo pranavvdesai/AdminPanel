268dcdd9735b6045959661cc1a0a34c490550795
pranavvdesai <pranavdesai.psd@gmail.com>
2021-12-21 01:26:21 +0530

    feat: box-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 128, 0, 0.5)',
+      }
+    },
   },
   variants: {
     extend: {},
