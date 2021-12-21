513bcd7f1b55a6c259dbc0aa52521c485a1e789f
pranavvdesai <pranavdesai.psd@gmail.com>
2021-12-22 03:47:22 +0530

    fix: UI table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -27,25 +27,25 @@ function Table({ students }) {
                     <table className="items-center w-full bg-transparent border-collapse">
                         <thead>
                             <tr>
-                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
+                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-semibold text-left">
                                     name
                                 </th>
-                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
+                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-semibold text-left">
                                     domain
                                 </th>
-                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
-                                    Status
+                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-semibold text-left">
+                                    Ssemibold
                                 </th>
-                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
+                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-semibold text-left">
                                     MCQ marks
                                 </th>
-                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
+                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-semibold text-left">
                                     Long Ans Marks
                                 </th>
-                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
+                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-semibold text-left">
                                     Total
                                 </th>
-                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
+                                <th className="px-2 text-purple-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-semibold text-left">
                                     Comments
                                 </th>
                             </tr>
@@ -53,29 +53,29 @@ function Table({ students }) {
                         <tbody>
                             {students.map(student => (
                         
-                        <tr>
-                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                        <tr className=" text-black">
+                                <th className="border-b border-gray-200 align-middle text-sm whitespace-nowrap px-2 py-4 text-left text-green-600 font-semibold">
                                     <Link to={location => ({ ...location, pathname: `${path}/${student.student.id}` })} >
                                         {student.student.name}
                                         </Link>
                                 </th>
-                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                <th className="border-b border-gray-200 align-middle text-sm whitespace-nowrap px-2 py-4 text-left font-semibold">
                                     {map[student.domain]}
                                 </th>
-                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                <th className="border-b border-gray-200 align-middle text-sm whitespace-nowrap px-2 py-4 text-left font-semibold">
                                     <i className="fas fa-circle fa-sm text-orange-500 mr-2"></i>{' '}
                                     pending
                                 </th>
-                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
+                                <th className="border-b border-gray-200 align-middle  text-sm whitespace-nowrap px-2 py-4 text-center  font-semibold">
                                     {student.MCQ_score}
                                 </th>
-                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-cenetr">
+                                <th className="border-b border-gray-200 align-middle  text-sm whitespace-nowrap px-2 py-4 text-cenetr font-semibold">
                                 {student.Long_Ans_Score}
                                     </th>
-                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
+                                <th className="border-b border-gray-200 align-middle  text-sm whitespace-nowrap px-2 py-4 text-center font-semibold">
                                 {student.Total}
                                     </th>
-                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                <th className="border-b border-gray-200 align-middle  text-sm whitespace-nowrap px-2 py-4 text-left font-semibold">
                                 {student.comments}
                                     </th>
                             </tr>
