505902494cb25958c503d22c6d12ee658212996d
pranavvdesai <pranavdesai.psd@gmail.com>
2021-12-25 02:12:05 +0530

    feat: structure first iteration

--- src/components/StudentDetails/StudentAnswers.jsx
+++ src/components/StudentDetails/StudentAnswers.jsx
@@ -44,29 +44,39 @@ function StudentAnswers({ answers }) {
     }
     console.log(checked)
     return (
-        <div>
-        <div className="flex flex-col justify-items-center align-middle mb-6 ">
+        <div className="mx-10">
+        <div className="flex flex-col justify-items-center align-middle mb-6 bg-gray-800 p-4 ">
                
-               <div>
-               <span className="mr-2 bg-green-400 border-2 ">Question:</span>   
-                  {answers.question.ques_main}
+                <div className="mb-1">
+                    <div className="flex flex-col justify-items-center align-middle">
+                    <span className="mr-2 border-b-2 border-green-700 text-green-800 rounded-md p-1 text-black">Question:</span>
+
+                            </div>
+                    <span>
+                        {/* {answers.question.ques_main} */}
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt fugit blanditiis corporis quos molestiae ratione error doloremque adipisci quia, deleniti, consequuntur eligendi, facilis explicabo? Voluptates asperiores minima quidem unde! Laudantium?
+
+                    </span>
                </div>
                <div className="flex ">
-                   <div>
-                       Answer: 
-                       {answers.answer}
+                    <div className="mt-2 mb-3">
+                            <span className="mr-3  bg-green-700 border-2 border-green-900 rounded-md p-1 text-black align-middle justify-items-center">Answer</span>
+                            
+                        {/* {answers.answer} */}
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, vel placeat impedit recusandae illum sed accusantium? Ab, beatae sit! Eum numquam voluptates repellendus. Odit rerum aperiam, nam cum aliquam earum.
                    </div>
                    
                    
                    
                 </div>
-                <div className="flex flex-col  ">
-               <div>
-                       Marks: 
-                       <input type="number" className="border border-yellow-500" onChange={(e) => setMarks(parseInt(e.target.value))}></input>
+                <div className="flex flex-col border-t-2 border-green-700 mb-2 ">
+               <div className=" mt-2">
+               <span className="mr-2 mt-2 rounded-md p-1 mr-7 ">Marks:</span>   
+                       <input type="number" className="borde rounded-md bg-black border-green-700 p-1 text-xs" placeholder="Enter the marks" onChange={(e) => setMarks(parseInt(e.target.value))}></input>
                    </div>
                    <div>
-                       checked?<input type="checkbox" className="border-green-500" onClick={
+                   <span className="mr-2    rounded-md p-1 ">Checked?</span>   
+<input type="checkbox" className="border-green-500" onClick={
                        click
                        } ></input>
                    </div>
