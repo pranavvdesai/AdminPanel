f54a1ed246f138260b772d9788f20fe993234b12
pranavvdesai <pranavdesai.psd@gmail.com>
2021-12-28 20:28:41 +0530

    fix: border length

--- src/pages/StudentDetails.jsx
+++ src/pages/StudentDetails.jsx
@@ -32,7 +32,7 @@ function StudentDetails() {
     ]);
     return (
         <div>
-            <h1 className=" mb-5 ml-3 text-green-700 semi-bold text-3xl border-b-4 border-green-700 p-2">Domains</h1>
+            <h1 className=" mb-5 ml-3 text-green-700 semi-bold text-3xl xl:w-1/6 w-1/2 lg:w-1/4 border-b-4 border-green-700 p-2">Domains</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-2 mx-3">
                 <Domains name={domains[0].name} id={domains[0].id} link={link}/>
                 <Domains name={domains[1].name} id={domains[1].id} link={link}/>
