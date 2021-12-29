aaeb617a0f74a4dd0a60651378e5f875c1dd3282
pranavvdesai <pranavdesai.psd@gmail.com>
2021-12-29 20:40:35 +0530

    feat: notistack init

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,20 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { SnackbarProvider } from 'notistack';
+import Slide from '@material-ui/core/Slide';
 
 const app = (
   <BrowserRouter>
+    <SnackbarProvider maxSnack={3}
+     anchorOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+  }}
+  TransitionComponent={Slide}
+    >
     <App />
+    </SnackbarProvider>
   </BrowserRouter>
 );
 
