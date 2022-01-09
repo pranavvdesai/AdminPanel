14704a2b3a43b0d5aaa63098ae6a7fee3a19afef
pranavvdesai <pranavdesai.psd@gmail.com>
2022-01-09 15:30:07 +0530

    fix: navbar responive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,54 @@
 import React from "react";
-
-export default function Navbar({ fixed }) {
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+import MenuIcon from '@mui/icons-material/Menu';
+import ViewSidebarIcon from '@mui/icons-material/ViewSidebar';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import CloseSharpIcon from '@mui/icons-material/CloseSharp';
+export default function Navbar({ fixed,showSidebar, setShowSidebar }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
+  const logout = () => {
+    localStorage.removeItem('HH');
+    window.location.href = '/';
+  };
   return (
     <>
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-green-700 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+          <div className="md:hidden">
+          <Button
+            color="transparent"
+            buttonType="link"
+            size="lg"
+            iconOnly
+            rounded
+            ripple="light"
+            onClick={() => setShowSidebar("left-0")}
+              >
+                {/* <ViewSidebarIcon /> */}
+                <ArrowForwardIosSharpIcon />
+            {/* <Icon name="menu" size="2xl" color="white" /> */}
+          </Button>
+          <div
+            className={`absolute top-2 md:hidden ${
+              showSidebar === "left-0" ? "left-56" : "-left-64"
+            } z-50 transition-all duration-300`}
+          >
+            <Button
+              color="transparent"
+              buttonType="link"
+              size="lg"
+              iconOnly
+              rounded
+              ripple="light"
+              onClick={() => setShowSidebar("-left-64")}
+                >
+                  <CloseSharpIcon />
+              {/* <Icon name="close" size="2xl" color="white" /> */}
+            </Button>
+          </div>
+        </div>
             <a
               className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
               href="#pablo"
@@ -17,7 +59,8 @@ export default function Navbar({ fixed }) {
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
-              <i className="fas fa-bars"></i>
+              <MenuIcon />
+              {/* <i className="fas fa-bars"></i> */}
             </button>
           </div>
           <div
@@ -29,12 +72,12 @@ export default function Navbar({ fixed }) {
           >
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
-                <a
+                <button
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#h"
+                  onClick={logout}
                 >
                   <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Logout</span>
-                </a>
+                </button>
               </li>
               
             </ul>
