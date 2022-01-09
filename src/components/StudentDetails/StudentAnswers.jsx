d64abf209c0e91ae7f9650b000b201557fa00037
pranavvdesai <pranavdesai.psd@gmail.com>
2022-01-09 22:06:32 +0530

    feat: marks on questions with errors handled

--- src/components/StudentDetails/StudentAnswers.jsx
+++ src/components/StudentDetails/StudentAnswers.jsx
@@ -3,11 +3,18 @@ import axios from 'axios';
 import Card from '@material-tailwind/react/Card';
 import CardHeader from '@material-tailwind/react/CardHeader';
 import CardBody from '@material-tailwind/react/CardBody';
-import {useLocation} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+import Slide from '@material-ui/core/Slide';
+import './../main.css'
+import arrow from '../../assets/images/arrow.svg'
+import { Link } from 'react-router-dom'
+
 function StudentAnswers({ answers }) {
-    const [checked, isChecked] = useState(false);
-    const [marks, setMarks] = useState(0);
-    const [id, setID] = useState(0);
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+    const [checked, isChecked] = useState(answers.is_checked);
+    const [marks, setMarks] = useState(answers.mark_ques);
+    const [id, setID] = useState(answers.question.id);
     const [btn, setColor] = useState('yellow');
     const location = useLocation();
     const { pathname } = location;
@@ -17,9 +24,14 @@ function StudentAnswers({ answers }) {
     var comments = "fnewfbjkebf"
     function click(){
         isChecked(!checked);
-        setID(answers.question.id)
     }
+    // function setid(e) {
+    //     // setID(answers.question.id)
+    //     console.log("hewerwer"+ answers.question.id)
+    //     setInterval(Eval(e),10000)
+    // }
     function Eval(e) {
+        console.log(id)
         e.preventDefault();
         axios.post(`https://recportal-iete.herokuapp.com/auth/adminmarklong/`, {
             user,
@@ -35,71 +47,119 @@ function StudentAnswers({ answers }) {
         })
             .then(response => {
                 if (response.data.message) {
-                    setColor('green');
-                    alert(response.data.message);
-                
+                    enqueueSnackbar('Marks Updated Succesfully', {
+                        variant: 'success',
+                        autoHideDuration: 2000,
+                        anchorOrigin: {
+                          vertical: 'top',
+                          horizontal: 'right',
+                        },
+                        TransitionComponent: Slide,
+                      })                
                 }
                 else {
+                    console.log("adsadsad"+response)
                     if (response.data.error) {
+                        console.log(response);
+                        enqueueSnackbar('Please fill up all the inputs', {
+                            variant: 'error',
+                            autoHideDuration: 2000,
+                            anchorOrigin: {
+                              vertical: 'top',
+                              horizontal: 'right',
+                            },
+                            TransitionComponent: Slide,
+                          })
                         console.log(response.data.error);
-                        setColor('red');
-                        alert(response.data.error);
                     }
                 }
                 console.log(response);
             })
             .catch(error => {
+                if (error.response.status === 403) { 
+                    enqueueSnackbar('Awarded marks cannot be more than max marks', {
+                        variant: 'error',
+                        autoHideDuration: 2000,
+                        anchorOrigin: {
+                          vertical: 'top',
+                          horizontal: 'right',
+                        },
+                        TransitionComponent: Slide,
+                      })
+                }
+                if (error.response.status === 404) { 
+                    enqueueSnackbar('Please fill in all the inputs', {
+                        variant: 'error',
+                        autoHideDuration: 2000,
+                        anchorOrigin: {
+                          vertical: 'top',
+                          horizontal: 'right',
+                        },
+                        TransitionComponent: Slide,
+                      })
+                }
                 console.log(error);
+                
             })
         
     }
     console.log(checked)
     return (
-        <div className="mx-10">
-        <div className="flex flex-col justify-items-center align-middle mb-6 bg-gray-800 p-4 ">
-               
-                <div className="mb-1">
+        <div className="mx-12">
+            
+        <div className="flex flex-col justify-items-center align-middle mb-6 p-4 card">
+        
+                {/* <div className="mb-1">
                     <div className="flex flex-col justify-items-center align-middle mb-1">
                     <span className="mr-2 border-b-2 border-green-700 text-green-600 rounded-md p-1">Question:</span>
 
                             </div>
                     <span>
-                        {/* {answers.question.ques_main} */}
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt fugit blanditiis corporis quos molestiae ratione error doloremque adipisci quia, deleniti, consequuntur eligendi, facilis explicabo? Voluptates asperiores minima quidem unde! Laudantium?
+                        {answers.question.ques_main}
 
                     </span>
-               </div>
+               </div> */}
                <div className="flex ">
                     <div className="mt-2 mb-3">
-                            <span className="mr-3  bg-green-700 border-2 border-green-900 rounded-md p-1 text-black align-middle justify-items-center">Answer</span>
+                    <span className="mr-2  text-green-600 rounded-md p-1">Question:</span>
+
                             
-                        {/* {answers.answer} */}
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, vel placeat impedit recusandae illum sed accusantium? Ab, beatae sit! Eum numquam voluptates repellendus. Odit rerum aperiam, nam cum aliquam earum.
+                            {answers.question.ques_main}
                    </div>
                    
                    
                    
                 </div>
-                <div className="flex flex-col border-t-2 border-green-700 mb-2 ">
+               <div className="flex ">
+                    <div className="mt-2 mb-3">
+                            {/* <span className="mr-3  bg-green-700 border-2 border-green-900 rounded-md p-1 text-black align-middle justify-items-center">Answer</span> */}
+                            <span className="mr-2  text-green-600 rounded-md p-1">Answer:</span>
+
+                        {answers.answer}
+                   </div>
+                   
+                   
+                   
+                </div>
+                {answers.question.ques_type === 1 && <div className="flex flex-col border-t-2 border-green-700 mb-2 ">
                <div className=" mt-2 mb-1">
                <span className="mr-2 mt-2 rounded-md p-1 mr-7 ">Marks:</span>   
-                       <input type="number" className="border-2 rounded-md bg-black border-green-700 p-1 text-xs" placeholder="Enter the marks" onChange={(e) => setMarks(parseInt(e.target.value))}></input>
+                       <input type="number" className="border-2 rounded-md bg-black border-green-700 p-1 text-xs" placeholder="Enter the marks" value={marks} onChange={(e) => setMarks(parseInt(e.target.value))}></input> / 10
                    </div>
                    <div>
                    <span className="mr-2    rounded-md p-1 ">Checked?</span>   
-<input type="checkbox" className="border-green-500" onClick={
+<input type="checkbox" className="border-green-500" checked={checked} onClick={
                        click
                        } ></input>
                    </div>
-                   {answers.question.ques_type === 1 && <button className={`bg-${btn}-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-24 mt-2`} onClick={Eval}>
+                    <button className={`bg-${btn}-500 text-white font-bold py-2 px-4 rounded w-24 mt-2`} onClick={(e) => { Eval(e);  }}>
                    Evaluate
-               </button>
-                    }
+                    </button>
                     </div>
+                    }
+                    
         </div>
                     
-     
-          
         </div>
     )
 }
